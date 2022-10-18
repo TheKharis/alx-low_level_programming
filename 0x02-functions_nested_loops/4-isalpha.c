bb72e5e528d9271efc2f4f04ad76d7c7bd1b57dd
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2022-10-18 20:51:24 +0000

    Clean code

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * _islower - checks for lowercase character
+  * _isalpha - checks for lowercase character
   * @c: The character to be checked.
   *
   * Return: 1 if character is lowercase, 0 otherwise.
