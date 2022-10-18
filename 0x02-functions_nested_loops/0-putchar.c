1783fdf1180402c9314ddfc48518c50364fe006b
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2022-10-18 19:41:46 +0000

    Task 0 code cleaned

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,9 @@
 /**
   * main - Entry point
   *
-  * Return: Always 0 (Success)
+  * Return: Always 0
   */
+
 int main(void)
 {
 	_putchar('_');
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-#ifndef main
-#define main
+#ifndef MAIN_M
+#define MAIN_M
 
 /*
  * File: main.h
- * 
+ *
  * Desc: Header file containing declarations
  */
 
