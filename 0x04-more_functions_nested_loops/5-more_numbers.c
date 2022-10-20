f0f5355959d209989ae8f3d2afb8d7d6900efc80
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2022-10-20 22:24:54 +0000

    Task 5 complete

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - function that prints 10 times the numbers
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		for (x = 0; x <= 14; x++)
+		{
+			if (x > 9)
+				_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+		}
+		_putchar('\n');
+
+	}
+}
