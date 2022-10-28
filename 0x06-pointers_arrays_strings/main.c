9725a9b091bdc37219b665181195559fbb311350
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2022-10-28 08:50:08 +0000

    Task 2 complete

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,9 +8,36 @@
  */
 int main(void)
 {
-	char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
-	printf("%s\n", buffer);
-	printf("---------------------------------\n");
-	print_buffer(buffer, sizeof(buffer));
+	char s1[98];
+	char *ptr;
+	int i;
+
+	for (i = 0; i < 98 - 1; i++)
+	{
+		s1[i] = '*';
+	}
+	s1[i] = '\0';
+	printf("%s\n", s1);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n",
+			5);
+	printf("%s\n", s1);
+	printf("%s\n", ptr);
+	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+	printf("%s", s1);
+	printf("%s", ptr);
+	for (i = 0; i < 98; i++)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", s1[i]);
+	}
+	printf("\n");
+
 	return (0);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+char *_strncpy(char *dest, char *src, int n);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_number(int n);
