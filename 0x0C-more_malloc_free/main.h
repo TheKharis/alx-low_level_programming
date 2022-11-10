23f1f3145c2f31f9c577fbb3d6d16810abe0a694
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2022-11-10 21:26:50 +0000

    Advanced task 2 complete

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
