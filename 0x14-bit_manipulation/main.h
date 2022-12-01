f33c56ce99e1122229d627bbb86418d2feba83ab
Reuben Agbenyegah <reubenagbenyegah86@gmail.com>
2022-12-01 21:06:55 +0000

    Task 0 complete

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdio.h>
+
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
+
+
+unsigned int binary_to_uint(const char *b);
+
+
+#endif
