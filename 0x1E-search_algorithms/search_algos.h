dc79cbd91e589fa92ac31041f6a9c2b7834a6256
TheKharis <reubenagbenyegah86@gmail.com>
2023-10-09 21:52:47 +0000

    Initial upload of advanced task 0 and 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -45,5 +45,7 @@ typedef struct skiplist_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
 
 #endif /* SEARCH_ALGOS_H */
