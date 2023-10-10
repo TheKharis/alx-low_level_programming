5703d84e3ab216f5e8f201015b4d0e9f013b74ee
TheKharis <reubenagbenyegah86@gmail.com>
2023-10-10 22:39:48 +0000

    Advanced task 5

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -51,6 +51,7 @@ int exponential_search(int *array, size_t size, int value);
 int _binary_search(int *array, size_t left, size_t right, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 
 #endif /* SEARCH_ALGOS_H */
